069a1739babde542c11cb2aed98ea68b79c52b9a
hemant_suman264 <hs938965@gmail.com>
2024-02-23 18:49:10 +0530

    first commit

---
+++ src/components/Tours.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Card from "./Card";
+
+function Tours({tours,removeTour}) {
+
+
+    return (
+        <div className="container">
+           <div>
+            <h2 className="title">Explore With Us</h2>
+           </div>
+
+           <div className="cards">
+           {
+            tours.map((tour) => {
+                return <Card {...tour} key={tour.id} removeTour={removeTour}></Card>
+            } )
+           } 
+           </div>
+        </div>
+    );
+}
+
+export default Tours;
